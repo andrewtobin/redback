699c3558f6bda0f680e8e90b60ef4fc8b7703539
andrew tobin <andrew@grrargh.com>
2010-12-23 23:32:38 +1100

    Took out most of the @mabster theme Added some settings in the couchdb database

--- site.rb
+++ site.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
 require 'haml'
+require 'couchrest'
 
-$siteTitle = 'Matt Hamilton'
+if ENV['CLOUDANT_URL']
+  set :db, CouchRest.database!( ENV['CLOUDANT_URL'] + '/redback' )
+  set :views, File.dirname(__FILE__) + '/views'
+  set :public, File.dirname(__FILE__) + '/public'
+else
+  set :db, CouchRest.database!( 'http://localhost:5984/redback' )
+end
 
 helpers do
   include Rack::Utils
@@ -9,10 +16,31 @@ helpers do
   def h(source)
     escape_html(source).gsub(' ', '%20')
   end
+  
+  def initiate_settings()
+    begin
+      @settings = options.db.get('settings')
+    rescue RestClient::ResourceNotFound
+      options.db.save_doc({'_id' => 'settings', :SiteTitle => 'Redback Site', :Author => 'Not set',
+                            :SiteDescription => 'Not set', :SiteKeywords => 'Not set'})
+      @settings = options.db.get('settings')
+    end
+        
+      $siteTitle = @settings['SiteTitle']
+      $author = @settings['Author']
+      $siteDescription = @settings['SiteDescription']
+      $siteKeywords = @settings['SiteKeywords']
+      end
 end
 
 set :haml, :format => :html5
 
+initialize
+
 get '/' do
-	haml :recent
+    initiate_settings
+    @posts = options.db.view('posts/all')['rows']
+    haml :recent
 end
+
+
