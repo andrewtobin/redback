714dfc1d8d7e4b60fec53dd5b6cfecebfa330b22
andrew tobin <andrew@grrargh.com>
2011-01-02 18:58:51 +1100

    Got site settings working, feeds administration, and new posts, next step work on getting feeds working, and revising posts.

---
+++ public/scripts/Site/Site.js
@@ -0,0 +1,84 @@
+﻿function enablePrettyPrinting() {
+    $("pre").each(
+        function () {
+            $(this).attr("class", "prettyprint");
+        });
+    prettyPrint();
+}
+
+function enablePrettyDates() {
+    $("span.date").each(function () {
+        var utc = $(this).html();
+        var utcDate = new Date(Date.parse(utc));
+
+        var utcDateValue = utcDate.getTime();
+        var localTimeOffset = utcDate.getTimezoneOffset() * 60000;
+
+        var localDate = new Date(utcDateValue - (localTimeOffset));
+        $(this).attr("title", Date.Format(localDate, "dd MMM, yyyy hh:nn"));
+
+        var pretty = prettyDate(localDate);
+        if (pretty == undefined) {
+            var local = Date.Format(localDate, "dd MMM, yyyy");
+            localTimeOffset / 360000;
+            $(this).html(local);
+        } else {
+            $(this).html(pretty);
+        }
+
+    });
+}
+
+var changes = 0;
+function enableChangeDetection() {
+    $(".promptBeforeUnload input").click(function () {
+        changes++;
+    });
+
+    $(".promptBeforeUnload textarea").click(function () {
+        changes++;
+    });
+
+    $("input[type=submit]").click(function () {
+        changes = 0;
+    });
+
+    window.onbeforeunload = function () {
+        if (changes > 0) {
+            return "Navigating away will lose the unsaved changes you have made.";
+        }
+    }
+}
+
+$.expr[':'].external = function (obj) {
+    return !(obj.href == "")
+            && !obj.href.match(/^mailto\:/)
+            && !(obj.hostname == location.hostname)
+};
+
+function enableNewWindowLinks() {
+    $(".content a:external")
+        .not(".comment-author a")
+        .addClass("new-window");
+
+    $('a.new-window').click(function () {
+        window.open(this.href);
+        return false;
+    });
+}
+
+function hideFlashes() {
+    $(".flash").delay(3000).fadeOut('slow');
+}
+
+$(function () {
+    enablePrettyPrinting();
+    enablePrettyDates();
+    enableNewWindowLinks();
+
+    if ($("#wmd-input").size() > 0) {
+        initializeWmd();
+    }
+    enableChangeDetection();
+});
+
--- site.rb
+++ site.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'haml'
 require 'couchrest'
+require 'uri'
+require 'net/http'
+require 'maruku'
 
 if ENV['CLOUDANT_URL']
   set :db, CouchRest.database!( ENV['CLOUDANT_URL'] + '/redback' )
@@ -10,24 +13,81 @@ else
   set :db, CouchRest.database!( 'http://localhost:5984/redback' )
 end
 
-$password = 'redback'
+$siteinitialized = false
+$itemsperpage = 30
 
 enable :sessions
 
 helpers do
   include Rack::Utils
 
+  def initializesite
+      if $siteinitialized == false
+        @settings = options.db.get('settings')
+        $siteTitle = @settings['SiteTitle']
+        $siteUrl = @settings['SiteUrl']
+        $author = @settings['Author']
+        $introduction = @settings['Introduction']
+        $siteDescription = @settings['SiteDescription']
+        $siteKeywords = @settings['SiteKeywords']
+        $mainLinks = @settings['MainLinks']
+        $siteFooter = @settings['SiteFooter']
+        $siteTheme = @settings['SiteTheme']
+        $uploadPath = @settings['UploadPath']
+        $akismetApiKey = @settings['AkismetApiKey']
+        $htmlhead = @settings['HtmlHead']
+        $htmlfoot = @settings['HtmlFoot']
+        $commentNotification = @settings['CommentNotification']
+        $smtpServer = @settings['SmtpServer']
+        $smtpPort = @settings['SmtpPort']
+        $smtpUseSsl = @settings['SmtpUseSsl']
+        $smtpFromEmailAddress = @settings['SmtpFromEmailAddress']
+        $smtpToEmailAddress = @settings['SmtpToEmailAddress']
+        $smtpUsername = @settings['SmtpUsername']
+        $smtpPassword = @settings['SmtpPassword']
+        $password = @settings['Password']
+        $siteinitialized = true
+      end
+  end
+  
+  def reinitializesite
+    $siteinitialized = false
+    initializesite
+  end
+  
   def h(source)
-    escape_html(source).gsub(' ', '%20')
+    escape_html(source).gsub(' ', '-')
   end
 
   def utcdatetime
     Time.now.utc
   end
   
+  def feedschecked(value)
+    value == true ? "checked" : ""
+  end
+  
+  def getposts(page)
+    numposts = options.db.view('posts/total_posts')['rows'].first['value']
+    pages = (numposts / $itemsperpage).ceil
+    posts = options.db.view('posts/by_postdate', {:skip => $itemsperpage * page, :limit => $itemsperpage, :descending => true})['rows']
+    return page, pages, posts
+  end
+  
+  def getthemes
+    directory = 'public/styles/themes/'
+    themes = Array.new
+    Dir.entries(directory).each do |d|
+        unless File.directory?(d) 
+            themes << d
+        end  
+    end
+    themes
+  end
+  
   def protected!
     unless authorized?
-      throw(:halt, [401, "Not authorized\n"])
+      redirect '/login'
     end
   end
 
@@ -50,28 +110,43 @@ helpers do
   end
 end
 
+class Entry
+    attr_accessor :Title, :MetaTitle, :PublishDate,
+        :Name, :Summary, :IsVisable, :Published, :Revisions, :Comments, :Pingbacks,
+        :MetaDescription, :MetaKeywords, :IsDiscussionEnabled, :IsNew,
+        :Reason, :Feeds, :Content
+        
+    def initialize
+        @Title = ''
+        @Name = ''
+        @Summary = ''
+        @Content = ''
+        @IsVisable = true
+        @Published = Time.now.utc
+        @Revisions = Array.new
+        @Comments = Array.new
+        @Pingbacks = Array.new
+        @Feeds = Array.new
+        @IsDiscussionEnabled = true
+        @IsNew = true
+    end
+end
+
+class Akismet
+    def validateKey
+        result = Net::HTTP.post_form(URI.parse('http://rest.akismet.com/1.1/verify-key'), {:key => $akismetApiKey, :blog => $siteUrl})
+        return result.body == 'valid'
+    end
+end
+
 set :haml, :format => :html5
 
-get '/' do
-    begin
-      @settings = options.db.get('settings')
-#      @settings = {'_id' => 'settings', :SiteTitle => 'Redback Site', :Author => 'Not set', :SiteDescription => 'Not set', :SiteKeywords => 'Not set'}
-      $siteTitle = @settings['SiteTitle']
-      $author = @settings['Author']
-      $siteDescription = @settings['SiteDescription']
-      $siteKeywords = @settings['SiteKeywords']
-      $siteKey = 'RedbackBlog'
-    rescue RestClient::ResourceNotFound
-      options.db.save_doc({'_id' => 'settings', :SiteTitle => 'Redback Site', :Author => 'Not set', :SiteDescription => 'Not set', :SiteKeywords => 'Not set'})
-      @settings = options.db.get('settings')
-      @settings = {'_id' => 'settings', :SiteTitle => 'Redback Site', :Author => 'Not set', :SiteDescription => 'Not set', :SiteKeywords => 'Not set'}
-      $siteTitle = settings['SiteTitle']
-      $author = settings['Author']
-      $siteDescription = settings['SiteDescription']
-      $siteKeywords = settings['SiteKeywords']
-    end
+before do
+    initializesite
+end
 
-    @posts = options.db.view('posts/all')['rows']
+get '/' do
+    @page, @pages, @posts = getposts(0)
     haml :recent
 end
 
@@ -79,9 +154,6 @@ get '/feed' do
 end
 
 get '/feeds/:name' do
-    protected!
-    @name = params[:name]
-    haml :test
 end
 
 get '/commentfeed' do
@@ -111,6 +183,66 @@ get '/admin' do
     haml :admin, :layout => :privateLayout
 end
 
+get '/admin/settings' do
+    protected!
+       
+    @themes = getthemes
+    @settings = options.db.get('settings')
+    haml :settings, :layout => :privateLayout
+end
+
+post '/admin/settings' do
+    protected!
+    @settings = options.db.get('settings')
+
+    @settings['SiteTitle'] = params[:SiteTitle]
+    @settings['SiteUrl'] = params[:SiteUrl]
+    @settings['Author'] = params[:Author]
+    @settings['Introduction'] = params[:Introduction]
+    @settings['SiteDescription'] = params[:SiteDescription]
+    @settings['SiteKeywords'] = params[:SiteKeywords]
+    @settings['MainLinks'] = params[:MainLinks]
+    @settings['SiteFooter'] = params[:SiteFooter]
+    @settings['SiteTheme'] = params[:SiteTheme]
+    @settings['UploadPath'] = params[:UploadPath]
+    @settings['AkismetApiKey'] = params[:AkismetApiKey]
+    @settings['HtmlHead'] = params[:HtmlHead]
+    @settings['HtmlFoot'] = params[:HtmlFoot]
+    @settings['CommentNotification'] = params[:CommentNotification]
+    @settings['SmtpServer'] = params[:SmtpServer]
+    @settings['SmtpPort'] = params[:SmtpPort]
+    @settings['SmtpUseSsl'] = params[:SmtpUseSsl]
+    @settings['SmtpFromEmailAddress'] = params[:SmtpFromEmailAddress]
+    @settings['SmtpToEmailAddress'] = params[:SmtpToEmailAddress]
+    @settings['SmtpUsername'] = params[:SmtpUsername]
+    @settings['SmtpPassword'] = params[:SmtpPassword]
+        
+    @settings.save()
+    
+    reinitializesite
+    redirect '/admin'
+end
+
+get '/admin/feeds' do
+    protected!
+       
+    @feeds = options.db.view('feeds/all')['rows']
+    haml :feeds, :layout => :privateLayout
+end
+
+post '/admin/feeds' do
+    options.db.save_doc({'_id' => h(params[:FeedName]), :Name => params[:FeedName], :Title => params[:FeedTitle], :Type => 'feed'})
+    redirect '/admin/feeds'
+end
+
+get '/admin/deletefeed/:feed' do
+    protected!
+    @feeds = options.db.get(params[:feed])
+    @feeds.destroy()
+    
+    redirect '/admin/feeds'
+end
+    
 get '/robots' do
 end
 
@@ -126,7 +258,45 @@ end
 get '/search' do
 end
 
+get '/restart' do
+    reinitializesite
+    redirect '/'
+end
+
 get '/new' do
+    protected!
+    @entry = Entry.new
+    @entry.Title = 'Enter a title'
+    @entry.IsDiscussionEnabled = true
+    @entry.MetaTitle = 'Enter a meta title'
+    @entry.PublishDate = Time.now.strftime('%Y-%m-%d')
+    @entry.IsNew = true
+    haml :edit, :layout => :privateLayout
+end
+
+post '/edit' do
+#    begin
+#        @entry = options.db.get(h(params[:Name]))
+#    rescue
+        @entry = Entry.new
+ #   end
+   
+    @entry.Name = params[:Name]
+    @entry.Title = params[:Title]
+    @entry.Summary = params[:Sidebar]
+    @entry.MetaTitle = params[:MetaTitle]
+    @entry.IsDiscussionEnabled = params[:AllowedComments]
+    @entry.MetaDescription = params[:MetaDescription]
+    @entry.MetaKeywords = params[:Keywords]
+    @entry.PublishDate = Time.now.utc
+    @entry.Content = params[:Content]
+    
+    options.db.save_doc({'_id' => h(params[:Name]), :Name => @entry.Name, :Title => @entry.Title, :Summary => @entry.Summary,
+        :PublishDate => @entry.PublishDate, :IsDiscussionEnabled => @entry.IsDiscussionEnabled, 
+        :MetaTitle => @entry.MetaTitle, :MetaDescription => @entry.MetaDescription, :MetaKeywords => @entry.MetaKeywords, 
+        :Content => @entry.Content, :Type => 'post'})
+
+    redirect '/' + h(params[:Name])
 end
 
 get '/edit/:page' do
@@ -135,10 +305,12 @@ end
 get '/revert/:page/:revision' do
 end
 
+
 get %r{/(\d+)} do
     begin
-      i = Integer(params[:captures].join)
-      'page number ' + i.to_s
+      page = Integer(params[:captures].join)
+      @page, @pages, @posts = getposts(page)
+      haml :recent
     rescue Exception::ArgumentError
       pass
     end    
@@ -151,6 +323,16 @@ get '/:page/revisions' do
 end
 
 get '/:page/:revision' do
+end
+
+get '/:pageTitle' do
+    begin
+        @entry = options.db.get(params[:pageTitle])
+        haml :entry
+    rescue
+        'Bee!'
+        #redirect not_found
+    end
 end
     
 not_found do
